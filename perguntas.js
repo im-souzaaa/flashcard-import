e7bfa43673e01f75af63d34482d49ba96332ca82
Andressa R. Souza <souza.andressa17@escola.pr.gov.br>
2026-06-26 17:58:36 -0300

    0

--- perguntas.js
+++ perguntas.js
@@ -1,11 +1,5 @@
 criaCartao(
     'Supernatural',
-    'Qual é o nome dos dois irmãos protagonistas da série?',
-    'Sam e Dean Winchester.'
-)
-
-criaCartao(
-    'Supernatural',
     'Qual é a marca e o ano do famoso carro preto conduzido por Dean?',
     'Chevrolet Impala 1967.'
 )
@@ -20,12 +14,6 @@ criaCartao(
     'Supernatural',
     'Como se chama o anjo que se torna um dos aliados mais importantes dos irmãos a partir da 4.ª temporada?',
     'Castiel.'
-)
-
-criaCartao(
-    'Supernatural',
-    'Qual é o nome do demônio de olhos amarelos que foi o principal antagonista nas primeiras temporadas e responsável pela morte da mãe deles?',
-    'Azazel.'
 )
 
 criaCartao(
