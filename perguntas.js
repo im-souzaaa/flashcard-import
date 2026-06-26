2b2e40d52ed5fb1b450a0c568af0bc6760152900
Andressa R. Souza <souza.andressa17@escola.pr.gov.br>
2026-06-26 17:48:59 -0300

    AAAAAAAAAAa

--- perguntas.js
+++ perguntas.js
@@ -1,24 +1,59 @@
 criaCartao(
-    'Programação',
-    'O que é Python?',
-    'O Python é uma linguagem de programação'
+    'Supernatural',
+    'Qual é o nome dos dois irmãos protagonistas da série?',
+    'Sam e Dean Winchester.'
 )
 
 criaCartao(
-    'Geografia',
-    'Qual a capital da França?',
-    'A capital da França é Paris'
+    'Supernatural',
+    'Qual é a marca e o ano do famoso carro preto conduzido por Dean?',
+    'Chevrolet Impala 1967.'
 )
 
 criaCartao(
-    'Programação',
-    'O que é uma função?',
-    'Uma função é um bloco de código que executa alguma tarefa'
+    'Supernatural',
+    'Qual é a icônica frase que resume o "negócio da família" dos Winchester?',
+    'Salvar pessoas, caçar coisas, o negócio da família.'
 )
 
 criaCartao(
-    'Lingua inglesa',
-    'Como se diz oi em Inglês?',
-    'Oi em ingles é HI (RAI)'
+    'Supernatural',
+    'Como se chama o anjo que se torna um dos aliados mais importantes dos irmãos a partir da 4.ª temporada?',
+    'Castiel.'
 )
 
+criaCartao(
+    'Supernatural',
+    'Qual é o nome do demónio de olhos amarelos que foi o principal antagonista nas primeiras temporadas e responsável pela morte da mãe deles?',
+    'Azazel.'
+)
+
+criaCartao(
+    'Supernatural',
+    'Que substância comum no dia a dia é usada pelos caçadores para afastar fantasmas e demânios e fazer barreiras)?',
+    'Sal.'
+)
+
+criaCartao(
+    'Supernatural',
+    'Como se chama o rei do Inferno de temperamento sarcástico que frequentemente oscila entre aliado e inimigo dos irmãos?',
+    'Crowley.'
+)
+
+criaCartao(
+    'Supernatural',
+    'Qual é o nome do amigável caçador veterano que serve como uma figura paterna e mentor para Sam e Dean?',
+    'Bobby Singer.'
+)
+
+criaCartao(
+    'Supernatural',
+    'Que música clássica do Kansas toca sempre no início de cada final de temporada "The Road So Far"?',
+    'Carry On Wayward Son.'
+)
+
+criaCartao(
+    'Supernatural',
+    'Qual é o nome da arma mística capaz de matar quase qualquer criatura sobrenatural, criada pelo próprio Samuel Colt?',
+    'O Colt.'
+)
